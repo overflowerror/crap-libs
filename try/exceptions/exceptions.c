c09f20d06b4de1bab384a6f9e63283a50f5f6c6d
overflowerror <mail@overflowerror.com>
2017-03-11 16:38:17 +0100

    some exceptions

--- try/exceptions/exceptions.c
+++ try/exceptions/exceptions.c
@@ -2,21 +2,54 @@
 
 #include <stdlib.h>
 
+class_t Exception_class;
 void method(Exception, destruct)(Exception_t* this) {
 	this->super.destruct((Object_t*) this);
 }
-
 void method(Exception, populate)(Exception_t* obj, class_t c) {
 	populate(Object)((Object_t*) obj, c);
 
 	add_method(obj, Exception, destruct);
 }
-
 Exception_t* method(Exception, construct)(const char* msg) {
 	Exception_t* obj = malloc(sizeof(Exception_t));
 	populate(Exception)(obj, Exception_class);
 
 	obj->msg = msg;
+
+	return obj;
+}
+
+
+class_t NullPointerException_class;
+void method(NullPointerException, destruct)(NullPointerException_t* this) {
+	this->super.destruct((Exception_t*) this);
+}
+void method(NullPointerException, populate)(NullPointerException_t* obj, class_t c) {
+	populate(Exception)((Exception_t*) obj, c);
+	
+	add_method(obj, NullPointerException, destruct);
+}
+NullPointerException_t* method(NullPointerException, construct)() {
+	NullPointerException_t* obj = malloc(sizeof(Exception_t));
+	populate(NullPointerException)(obj, NullPointerException_class);
+
+	return obj;
+}
+
+
+class_t OutOfMemoryException_class;
+void method(OutOfMemoryException, destruct)(OutOfMemoryException_t* this) {
+	this->super.destruct((Exception_t*) this);
+}
+void method(OutOfMemoryException, populate)(OutOfMemoryException_t* obj, class_t c) {
+	populate(Exception)((Exception_t*) obj, c);
+	
+	add_method(obj, OutOfMemoryException, destruct);
+}
+OutOfMemoryException_t* method(OutOfMemoryException, construct)() {
+	OutOfMemoryException_t* obj = malloc(sizeof(Exception_t));
+	populate(OutOfMemoryException)(obj, OutOfMemoryException_class);
 
 	return obj;
 }
--- try/exceptions/exceptions.h
+++ try/exceptions/exceptions.h
@@ -4,7 +4,7 @@
 #include "../../oop/oop.h"
 
 #define Exception construct(Exception)
-class(Exception, Object_class, NO_INTERFACES, true) {
+extern class(Exception, Object_class, NO_INTERFACES, true) {
 	extends(Object_t);	
 
 	const char* msg;
@@ -14,5 +14,25 @@ class(Exception, Object_class, NO_INTERFACES, true) {
 
 void method(Exception, populate)(Exception_t*, class_t);
 Exception_t* method(Exception, construct)(const char*);
+
+#define NullPointerException construct(NullPointerException)
+extern class(NullPointerException, Exception_class, NO_INTERFACES, true) {
+	extends(Exception_t);
+
+	void (*destruct)(defclass NullPointerException*);
+} NullPointerException_t;
+
+void method(NullPointerException, populate)(NullPointerException_t*, class_t);
+NullPointerException_t* method(NullPointerException, construct)(void);
+
+#define OutOfMemoryException construct(OutOfMemoryException)
+extern class(OutOfMemoryException, Exception_class, NO_INTERFACES, true) {
+	extends(Exception_t);
+
+	void (*destruct)(defclass OutOfMemoryException*);
+} OutOfMemoryException_t;
+
+void method(OutOfMemoryException, populate)(OutOfMemoryException_t*, class_t);
+OutOfMemoryException_t* method(OutOfMemoryException, construct)(void);
 
 #endif
